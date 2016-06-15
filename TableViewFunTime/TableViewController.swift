60cf6d61c3e9ad36d72ab7ea2f52b97de5eb230c
JimCampagno <jimcampagno@gmail.com>
2016-06-15 18:04:55 -0400

    add xcode project

---
+++ TableViewFunTime/TableViewController.swift
@@ -0,0 +1,69 @@
+//
+//  TableViewController.swift
+//  TableViewFunTime
+//
+//  Created by James Campagno on 6/15/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+    
+    var favoriteSongs: [String] = []
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        generateFavoriteSongs()
+        
+    }
+    
+    func generateFavoriteSongs() {
+
+        favoriteSongs = [
+            "Thriller",
+            "Never Gonna Give You Up",
+            "Safety Dance",
+            "Space Oddity",
+            "Smells Like Teen Spirit",
+            "Jealous Guy",
+            "Jolene",
+            "Moondance",
+            "Stairway To Heaven",
+            "One",
+            "Life On Mars?"
+        ]
+        
+    }
+    
+    
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        
+        return 1
+        
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return favoriteSongs.count
+        
+    }
+
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("basicCell", forIndexPath: indexPath)
+
+        let favoriteSong = favoriteSongs[indexPath.row]
+        
+        cell.textLabel?.text = favoriteSong
+
+        return cell
+    }
+ 
+
+}
